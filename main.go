34b267d20e81d8699f72b9476fa1ac12bc57903e
vasishthroy <royvasishth99@gmail.com>
2024-12-19 00:19:45 +0530

    Updated the function comment description

--- main.go
+++ main.go
@@ -5,29 +5,32 @@ import (
 	"strings"
 )
 
+// Declaring variables outside the main function
+// makes the variable accessible across the script
+const conferenceTickets = 50
+
+var conferenceName = "Go conference"
+var remainingTickets uint = conferenceTickets
+
+// Using array to store the data of users booking the ticket
+
+// Declare an empty array of size 50 of string type
+// var bookings = [50]string{}
+
+// Declaring an array with prefilled values
+// var bookings = [50]string{"VR", "AP", "TT"}
+
+// Alternate syntax for declaring Array
+// var bookings [conferenceTickets]string
+
+// If the size is kept blank then it becomes a Slice
+// They are more efficient than arrays as they automatically allocate memory
+// based on the amount of value being stored
+var bookings []string
+
 func main() {
-	// Alternative Syntax of creating a variable with predefined value
-	conferenceName := "Go conference"
-	const conferenceTickets = 50
-	var remainingTickets uint = conferenceTickets
 
-	// Using array to store the data of users booking the ticket
-
-	// Declare an empty array of size 50 of string type
-	// var bookings = [50]string{}
-
-	// Declaring an array with prefilled values
-	// var bookings = [50]string{"VR", "AP", "TT"}
-
-	// Alternate syntax for declaring Array
-	// var bookings [conferenceTickets]string
-
-	// If the size is kept blank then it becomes a Slice
-	// They are more efficient than arrays as they automatically allocate memory
-	// based on the amount of value being stored
-	var bookings []string
-
-	greetUser(conferenceName, conferenceTickets)
+	greetUser()
 
 	// A for loop executes until the condition for the loop is satisfied
 	// eg. for userTickets <= 0 {}
@@ -36,49 +39,20 @@ func main() {
 	// 2) for {}
 	for {
 
+		// Get user details and ticket count
 		firstName, lastName, emailAddress, userTickets := getUserInput()
 
-		var isValidName, isValidEmail, isValidTicketNumber bool = validateInput(firstName,
-			lastName,
-			emailAddress,
-			userTickets,
-			remainingTickets)
+		// Check for any invalid details provided by user
+		var isValidName,
+			isValidEmail,
+			isValidTicketNumber bool = validateInput(firstName, lastName, emailAddress, userTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
-			remainingTickets = remainingTickets - uint(userTickets)
-			// bookings[0] = firstName + " " + lastName
-			// Slice uses the built-in append function to append data to it
-			bookings = append(bookings, firstName+" "+lastName)
 
-			// Check for the array or slice data logged
-			/*
-				fmt.Printf("Array / Slice: %v\n", bookings)
-				fmt.Printf("Array / Slice first element: %v\n", bookings[0])
-				fmt.Printf("Array / Slice type: %T\n", bookings)
-				fmt.Printf("Array / Slice length: %v\n", len(bookings))
-			*/
-
-			// Ask for users name and no. of tickets
-			fmt.Printf("Thank you %v %v for booking %v tickets. ",
-				firstName, lastName, userTickets)
-			fmt.Printf("You will receive a confirmation on %v soon.\n",
-				emailAddress)
-			fmt.Printf("Remaining tickets: %v\n", remainingTickets)
+			bookTickets(firstName, lastName, emailAddress, userTickets)
 
 			var firstNames []string
-
-			// Iterative for loop
-			// range lets us iterate over elements
-			// for slices and array it provides index and element
-			// Go interprets "_" (blank identifier)  as an unused variable
-			for _, name := range bookings {
-
-				// stings module has a method called Fields
-				// It splits the string based on white spaces available
-				var firstName = strings.Fields(name)
-				firstNames = append(firstNames, firstName[0])
-			}
-
+			firstNames = getFirstNames(firstNames)
 			fmt.Printf("This is all our current booking: %v\n", firstNames)
 
 			if remainingTickets == 0 {
@@ -106,19 +80,18 @@ func main() {
 	}
 }
 
-func greetUser(confName string, confTickets int) {
+func greetUser() {
 	/**
-	This function is used ot print the greeting and information message
-	it takes 3 arguments
-	confName (string): The name of the conference for which the ticket is being sold.
-	confTickets (int): Total number of tickets that are available for booking.
+		This function is used ot print the greeting and information message
+		It doesn't take any argument
+		It doesn't return any value.
 	**/
 
 	// Println ends with a \n
-	fmt.Println("Welcome to our", confName, "booking application")
+	fmt.Println("Welcome to our", conferenceName, "booking application")
 	// "Printf" is Print with format option for adding variables
 	// to display variables with the print text
-	fmt.Printf("We have a total of %v tickets\n", confTickets)
+	fmt.Printf("We have a total of %v tickets\n", conferenceTickets)
 	fmt.Println("Get your tickets here to attend")
 }
 
@@ -126,6 +99,13 @@ func greetUser(confName string, confTickets int) {
 // Although for returning multiple function it needs to be defined
 // SYNTAX: func functionName() (return datatype1, return datatype2, ...) {}
 func getUserInput() (string, string, string, int) {
+	/*
+		Function is used to get data from the user like
+		First name, last name, email address and no. of tickets the user want to book
+		This function doesn't take any argument and returns 4 values
+		Return type: string, string, string and int
+	*/
+
 	var firstName string
 	var lastName string
 	var emailAddress string
@@ -148,19 +128,79 @@ func getUserInput() (string, string, string, int) {
 	return firstName, lastName, emailAddress, userTickets
 }
 
-func validateInput(firstName string, lastName string, emailId string, ticketNeeded int, ticketRemaining uint) (bool, bool, bool) {
+func validateInput(firstName string, lastName string, emailId string, ticketNeeded int) (bool, bool, bool) {
 	/*
 		Check if user has not provided invalid data
-		It takes 5 arguments
-		firstName (string): First name input by user.
-		lastName (string): Last name input by user.
-		emailId (string): Email id input by user.
-		ticketNeeded (int): Number of ticket user needs to book.
-		ticketRemaining (uint): Available tickets from which user can book.
+		Arguments:
+			firstName (string): First name of the user.
+			lastName (string): Last name of the user.
+			emailId (string): Email id of the user.
+			ticketNeeded (int): No. of ticket user needs.
+		Return type: bool, bool and bool
+
 	*/
 
 	isValidName := len(firstName) > 2 && len(lastName) > 2
 	isValidEmail := strings.Contains(emailId, "@")
-	isValidTicketNumber := ticketNeeded > 0 && ticketRemaining >= uint(ticketNeeded)
+	isValidTicketNumber := ticketNeeded > 0 && remainingTickets >= uint(ticketNeeded)
 	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func bookTickets(firstName string, lastName string, emailId string, userTickets int) {
+	/*
+		This function is used to log the booking done by the user.
+		It subtracts the tickets booked by the user from the remaining ticket count
+		and creates a log by saving the name of the user inside the booking
+		Arguments:
+			firstName (string): First Name of the user
+			lastName (string): Last Name of the user
+			emailId (string): Email of the user
+			userTickets (int): No. of tickets the user wants to book.
+		This function doesn't return any value.
+	*/
+
+	remainingTickets = remainingTickets - uint(userTickets)
+
+	// bookings[0] = firstName + " " + lastName
+	// Slice uses the built-in append function to append data to it
+	bookings = append(bookings, firstName+" "+lastName)
+
+	// Check for the array or slice data logged
+	/*
+		fmt.Printf("Array / Slice: %v\n", bookings)
+		fmt.Printf("Array / Slice first element: %v\n", bookings[0])
+		fmt.Printf("Array / Slice type: %T\n", bookings)
+		fmt.Printf("Array / Slice length: %v\n", len(bookings))
+	*/
+
+	// Ask for users name and no. of tickets
+	fmt.Printf("Thank you %v %v for booking %v tickets. ",
+		firstName, lastName, userTickets)
+	fmt.Printf("You will receive a confirmation on %v soon.\n", emailId)
+	fmt.Printf("Remaining tickets: %v\n", remainingTickets)
+}
+
+func getFirstNames(firstNames []string) []string {
+
+	/*
+		This function splits the names present inside the booking slice
+		into first name and last name, and then saves the fist name
+		into the slice argument which is then returned.
+		Arguments:
+			firstNames (slice string): An empty slice variable
+		Return type: slice string
+	*/
+
+	// Iterative for loop
+	// range lets us iterate over elements
+	// for slices and array it provides index and element
+	// Go interprets "_" (blank identifier)  as an unused variable
+	for _, name := range bookings {
+
+		// stings module has a method called Fields
+		// It splits the string based on white spaces available
+		var firstName = strings.Fields(name)
+		firstNames = append(firstNames, firstName[0])
+	}
+	return firstNames
 }
